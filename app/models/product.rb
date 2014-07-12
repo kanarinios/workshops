8b4c2a321f7c4e653337236b137c1aaa6bb94066
kanarinios <dariusz.bliszczyk@gmail.com>
2014-07-12 16:24:10 +0200

    Added validations to product model. Added average_rating method

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,16 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+  validates :title, :description, :price, presence: true
+  validates :price, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }
+  
+  def average_rating
+    reviews = self.reviews
+    avg = 0
+    
+    reviews.each do |r|
+      avg = avg + r.rating
+    end
+    avg = avg/reviews.count.to_f
+  end
 end
