6ae6dcd8d2970e92fe4d061eace2e841e81ce5b1
kanarinios <dariusz.bliszczyk@gmail.com>
2014-07-12 11:47:03 +0200

    fixing step one, user specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  attr_accessor(:firstname, :lastname)
+  validates :firstname,:lastname, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
