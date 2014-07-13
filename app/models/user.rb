790b43c4984340190a03d251130569f54aefb1e1
kanarinios <dariusz.bliszczyk@gmail.com>
2014-07-13 22:16:50 +0200

    fix user model setters and getters

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
-  attr_accessor(:firstname, :lastname, :email, :password, :password_confimation)
   validates :firstname,:lastname, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
